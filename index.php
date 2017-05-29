1508d5d731e3ccd477cbacd931c8a8a73e9259b7
Alexander <antidze.sasha@gmail.com>
2017-05-29 19:10:04 +0400

    taking css to external stylesheet

--- index.php
+++ index.php
@@ -5,60 +5,7 @@
 	<head>
 		<title>Audio Player</title>
 		<link rel="stylesheet" href="css/main.css" type="text/css">
-		<style>
-		
-			.login{
-				width: 245px;
-				margin: 0 auto;
-				padding-top: 75px;
-			}
-			
-			.login h1{
-				border: 1px solid #fff;
-				padding: 10px;
-				font-size: 13px;
-				margin-bottom: 20px;
-			}
-			
-			.login form h2{
-				margin-left: 90px;
-			}
-			
-			.login form input{
-				padding-top: 15px;
-				padding-bottom: 15px;
-				width: 245px;
-			}
-			
-			.login form input[type]{
-				font-size: 18px;
-			}
-			
-			.login-email, .login-password, .login-submit{
-				margin-top: 15px;
-			}
-			
-			.login-submit input[value]{
-				font-weight: bold;
-				font-size: 18px;
-			}
-			
-			.register-link{
-				margin-top: 15px;
-			}
-			
-			.register-link a{
-				color: #fff;
-				font-size: 16px;
-				text-decoration: none;
-			}
-			
-			.register-link a:hover{
-				color: cyan;
-			}
-			
-		
-		</style>
+	
 	</head>
 	<body>
 		<div id="container" style="width: 500px;height: 600px;">
