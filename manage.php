f78b733aedf6c348147f2ece43726079a5a2fc1c
Alexander <antidze.sasha@gmail.com>
2017-06-04 19:57:38 +0400

    moving jquery code to external js file

--- manage.php
+++ manage.php
@@ -153,38 +153,7 @@ if($db_author != $sess_author){
 		
 		<script src="js/jquery.js"></script>
 		<script src="js/slide.js"></script>
-		<script>
-			$('.changename a').click(function() {
-				$(this).fadeOut('medium', function() {
-					//var track = '';
-					$('.changename').append(
-						"<form action='includes/change_title.php' method='post'>" +
-							"<input type='text' name='new_name' value='<?php echo $trackname; ?>'>" +
-							"<input type='hidden' name='playlist_id' value='<?php echo $playlist; ?>'>" +
-							"<input type='hidden' name='track_id' value='<?php echo $track_id; ?>'>" +
-							"<input type='submit' name='change' value='Change'>" +							 
-						"</form>"
-					).hide().fadeIn('fast');
-					
-				});
-			});
-			
-			$('.changename').mouseenter(function() {
-				$("input[value='Change']").css({"background": "darkcyan", "color": "black"});
-			});
-			
-			$('.changename').mouseleave(function() {
-				$("input[value='Change']").css({"background": "black", "color": "white"});
-			});
-			
-			
-			$('#movetr').hide();
-			
-			$('.move-options select').on('change', function() {
-				$('#movetr').fadeIn('slow');
-			});
-			
-		</script>
+		
 		
 
 	</body>
