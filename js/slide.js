1527e1b342c38fda4c59b759b0c08f469a281259
Alexander <antidze.sasha@gmail.com>
2017-06-04 19:58:08 +0400

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -11,6 +11,34 @@ $(document).ready(function() {
 	});
 	
 	
-	
+	$('.changename a').click(function() {
+				$(this).fadeOut('medium', function() {
+					//var track = '';
+					$('.changename').append(
+						"<form action='includes/change_title.php' method='post'>" +
+							"<input type='text' name='new_name' value='<?php echo $trackname; ?>'>" +
+							"<input type='hidden' name='playlist_id' value='<?php echo $playlist; ?>'>" +
+							"<input type='hidden' name='track_id' value='<?php echo $track_id; ?>'>" +
+							"<input type='submit' name='change' value='Change'>" +							 
+						"</form>"
+					).hide().fadeIn('fast');
+					
+				});
+			});
+			
+			$('.changename').mouseenter(function() {
+				$("input[value='Change']").css({"background": "darkcyan", "color": "black"});
+			});
+			
+			$('.changename').mouseleave(function() {
+				$("input[value='Change']").css({"background": "black", "color": "white"});
+			});
+			
+			
+			$('#movetr').hide();
+			
+			$('.move-options select').on('change', function() {
+				$('#movetr').fadeIn('slow');
+			});
 	
 });
